3b067d115c72e848b16f9e0ba8b7e9c4e1fa7578
jerrettl <jerrett7@gmail.com>
2022-04-11 21:16:30 -0400

    Add build_all.sh script
    This script is of its first iteration, where it can convert all
    markdown files within the src/ directory into HTML files in
    an output directory.
    
    Contributes to #21.

---
+++ build/build_all.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+# Created by Jerrett Longworth
+
+# Setup variables
+GREY='\033[1;30m'
+RESET='\033[0m'
+SCRIPT_LOCATION=$(readlink -e "$0")
+SCRIPT_LOCATION=${SCRIPT_LOCATION%/*}
+
+CONVERT_SCRIPT=$(readlink -e "$SCRIPT_LOCATION/md_to_html.sh")
+INPUT_DIR="$SCRIPT_LOCATION/../src/" 
+OUTPUT_DIR="$SCRIPT_LOCATION/../out/"
+
+
+show_usage() {
+	cat << EOF
+Usage: ${0##*/} [-sv]
+Builds all markdown files.
+
+Options:
+  -s      Suppress all output from secondary build script.
+  -v      Show all logs.
+EOF
+}
+
+
+# Test for command line options
+suppress=0
+verbose=0
+OPTIND=1
+while getopts sv option; do
+	case $option in
+    s)
+      suppress=1
+      ;;
+    v)
+      verbose=1
+      ;;
+    *)
+      echo "Error: Unknown argument."
+      show_usage
+      exit -1
+  esac
+done
+shift "$((OPTIND-1))"
+
+# Read the list of all markdown files to process
+files=$(find ${INPUT_DIR} -type f -name "*.md" -printf "%P\n")
+total=$(echo "$files" | wc -l)
+
+
+# Build each file
+i=1
+for file in $files; do
+  # Split the file into its path and filename components
+  filepath="${file%/*}"
+  filename="${file##*/}"
+
+  # Set the output file
+  output_file="$OUTPUT_DIR$filepath/${filename%%.md}.html"
+
+  echo " -> [$i/$total] $file"
+
+  mkdir -p "$OUTPUT_DIR$filepath"
+
+  # Temporarily enter the directory the file is in for relative-path links
+  pushd "$INPUT_DIR$filepath" 2>/dev/null > /dev/null
+  echo -en "$GREY"
+
+  # Build the HTML file based on the command line arguments
+  if [ $suppress -eq 1 ]; then
+    # Suppress all output.
+    "$CONVERT_SCRIPT" -h -o "$output_file" "$INPUT_DIR$file" 2>/dev/null > /dev/null
+  elif [ $verbose -eq 1 ]; then
+    # Show all output.
+    "$CONVERT_SCRIPT" -h -o "$output_file" "$INPUT_DIR$file"
+  else
+    # Only show warnings and errors.
+    "$CONVERT_SCRIPT" -h -o "$output_file" "$INPUT_DIR$file" > /dev/null
+  fi
+
+  # Restore the working directory
+  echo -en "$RESET"
+  popd 2>/dev/null > /dev/null
+
+  i=$((i + 1))
+done
