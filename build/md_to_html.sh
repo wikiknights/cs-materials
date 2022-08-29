f640903598eea653a4406d109d863e46b5a638c0
Jerrett Longworth <jerrett7@gmail.com>
2022-08-29 10:08:42 -0400

    Include linewrap.tex only in PDF output
    Signed-off-by: Jerrett Longworth <jerrett7@gmail.com>

--- build/md_to_html.sh
+++ build/md_to_html.sh
@@ -75,7 +75,7 @@ convert_pdf() {
 	fi
 
 	echo "Converting to PDF..."
-	pandoc tmp.md -o "$outputfile" --highlight-style "$ROOT_DIR/templates/customhighlight.theme" --defaults "$ROOT_DIR/build/defaults.yaml"
+	pandoc tmp.md -o "$outputfile" --highlight-style "$ROOT_DIR/templates/customhighlight.theme" --defaults "$ROOT_DIR/build/defaults.yaml" --include-in-header "$ROOT_DIR/build/linewrap.tex"
 }
 
 converter() {
